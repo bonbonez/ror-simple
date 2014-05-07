67eeed919f7eed2eeab99db70e0f58cf44247a6f
Artem Petrov <bonbonez@gmail.com>
2014-05-07 23:19:26 +0400

    minor changes

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+class IndexController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+
+  def index
+
+    #return render text: "hello"
+    render layout: 'application'
+  end
+
+end
